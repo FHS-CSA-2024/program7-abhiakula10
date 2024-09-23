b29fe1d472c8f9d4b3ca0a1e7fb45f255be0fc0d
abhiakula10 <abhinavraj8258@gmail.com>
2024-09-22 18:59:35 -0700

    Update Program.java
    additional changes

--- Program.java
+++ Program.java
@@ -3,32 +3,24 @@ import java.util.Scanner;
 public class Program7 {
   
 public static void main(String[] args){
-  
     double sbuck;
-
     double klevins;
-
     double snickels;
-
     double newSbuck
 
     Scanner newNum = new Scanner(System.in);
     
-    System.out.print("Enter schrute-bucks: ");
-    
+  System.out.print("Enter the Schrute-Bucks: ");
     sbuck = newNum.nextDouble();
     
-    System.out.print("Enter klevins: ");
-    
+  System.out.print("Enter thr Klevins: ");
     klevins = newNum.nextDouble();
     
-    System.out.print("Enter stanley-nickels");
-    
+  System.out.print("Enter the STanley-Nickels");
     snickels = newNum.nextDouble();
-    
-    newSbuck = sbuck + klevins*(1.0/20) + snickels*(1.0/240);
+    newSbuck = sbuck + klevins*( 1.0 / 20 ) + snickels*( 1.0 / 240 );
    
-    System.out.println(newSbuck);
+  System.out.println(newSbuck);
 }
 
 
